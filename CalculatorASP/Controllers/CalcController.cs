b5d212df6f5a097196fc665f577e6eb1bd867466
DieselRost <rharvanko@gmail.com>
2017-06-28 12:14:14 +0300

    Add simple parse

--- CalculatorASP/Controllers/CalcController.cs
+++ CalculatorASP/Controllers/CalcController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Web.Http;
+using System;
+using System.Collections.Generic;
+
 
 namespace CalculatorASP.Controllers
 {
@@ -7,9 +10,31 @@ namespace CalculatorASP.Controllers
         // POST api/<controller>
         public decimal Post([FromBody]Expression exp)
         {
-            // TODO: parse exp
-            // TODO: calc results
-            var result = 10.25M;
+            string thisexp = Convert.ToString(exp);
+            string[] stringmass = thisexp.Split('+', '-', '÷', 'x');
+            List < decimal > numbers= new List<decimal>();
+            foreach (var elements in stringmass)
+            {
+                numbers.Add(Convert.ToDecimal(elements));
+            }
+            bool add = thisexp.Contains("+");
+            bool sbc = thisexp.Contains("-");
+            bool mul = thisexp.Contains("x");
+            bool div = thisexp.Contains("÷");
+            var result = numbers[0];
+            if (add)
+            {
+                return numbers[0] + numbers[1];
+            } else if (sbc)
+            {
+                return numbers[0] - numbers[1];
+            } else if (mul)
+            {
+                return numbers[0] * numbers[1];
+            } else if (div)
+            {
+                return numbers[0] / numbers[1];
+            } else
             return result;
         }
     }
