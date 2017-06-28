ee8affca70a36183da5523126be53accaf68f93e
Mykola Kohut <kohut.mykola@gmail.com>
2017-06-28 14:34:28 +0300

    added validation and textarea

--- CalculatorASP/Scripts/script.js
+++ CalculatorASP/Scripts/script.js
@@ -1,22 +1,36 @@
 ﻿function calc() {
+    var input = document.getElementById("inp").value;
+    if (isInArray(input.substr(input.length - 1), operations)) {
+        input = input.slice(0, -1);
+    }
     $.ajax({
         type: "POST",
-        dataType: 'json',
-        contentType: 'application/json',
-        url: '/api/calc',
+        dataType: "json",
+        contentType: "application/json",
+        url: "/api/calc",
         data: JSON.stringify({
-            "Input": document.getElementById("inp").value,
-         
+            "Input": input
         }),
-        success: function (data) { document.getElementById("inp").value = data; },
-        error: function () { alert("error"); }
+        success: function(data) { document.getElementById("inp").value = data; },
+        error: function() { alert("error"); }
     });
 }
 
-function numberClick(number) {
-    document.getElementById("inp").value += number;
+function numberClick(symbol) {
+    var input = document.getElementById("inp").value;
+    if (isInArray(symbol, operations) && isInArray(input.substr(input.length - 1), operations)) {
+        return;
+    } else {
+        document.getElementById("inp").value += symbol;
+    }
 }
 
 function clearInput() {
-    document.getElementById("inp").value = '';
+    document.getElementById("inp").value = "";
 }
+
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+
+var operations = ["+", "-", "*", "/"];
